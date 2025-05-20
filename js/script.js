c161770c678de99898a477500187044fb82a2e35
localbody-ic <localbody@mail.ru>
2025-05-20 17:43:24 +0300

    popup

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
 const onLoaded = () => {
-  // accordion
+  // закроем открытый popup
+  const closePopup = function (event) {
+    const popup = document.querySelector('.popup-toggle.open')
+    // если кликнули не по самому popup
+    if (popup && !event.target.closest('.popup-toggle')) {
+      popup?.classList.remove('open')
+    }
+  }
 
+  document.addEventListener('click', closePopup)
+
+  // .subnav .city-selector
+  const citySelector = document.querySelector('.subnav .city-selector')
+
+  const onClickCitySelector = function (event) {
+    citySelector?.classList.toggle('open')
+  }
+
+  citySelector?.addEventListener('click', onClickCitySelector)
+
+  // end .subnav .city-selector
+
+  // accordion
   const accordions = document.querySelectorAll('.accordion')
 
   const onClickAccordionItem = function (event) {
@@ -11,6 +32,7 @@ const onLoaded = () => {
   accordions?.forEach((item) => {
     item.addEventListener('click', onClickAccordionItem)
   })
+  // end accordion
 }
 
 document.addEventListener('DOMContentLoaded', onLoaded)
