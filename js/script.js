75b150c6b8b07b99bed75069e7afe9a8a2fe3296
localbody macmini <localbody@gmail.com>
2025-05-24 14:45:07 +0300

    catalog in header

--- js/script.js
+++ js/script.js
@@ -1,4 +1,56 @@
 const onLoaded = () => {
+  //catalog-in-header
+  const COUNT_OF_CATALOG_ITEMS_TO_SHOW = 10
+  const catalogInHeader = document.querySelector('.catalog-in-header .catalog')
+  const listUL = catalogInHeader.querySelectorAll('ul')
+
+  const buttonCatalogOpen = document.querySelector('.catalog-open')
+
+  const onClickButtonCatalogOpen = () => {
+    closePopup()
+    buttonCatalogOpen.closest('.popup-toggle').classList.toggle('open')
+  }
+
+  buttonCatalogOpen.addEventListener('click', onClickButtonCatalogOpen)
+
+  // свернем в каталоге строки, где больше 10 строк
+  // если нет более 10 строк - скроем кнопку ЕЩЕ
+
+  listUL.forEach((item) => {
+    // debugger
+    let isShowButtonMore = false
+    item.querySelectorAll('li.item').forEach((itemLI, index) => {
+      if (index > COUNT_OF_CATALOG_ITEMS_TO_SHOW) {
+        itemLI.classList.add('item--hidden')
+        isShowButtonMore = true
+      }
+    })
+
+    if (!isShowButtonMore) {
+      item.querySelector('li.more').style.display = 'none'
+    }
+  })
+
+  const listButtonMore = catalogInHeader.querySelectorAll('.more')
+
+  listButtonMore.forEach((item) => {
+    const onClickButtonMore = (event) => {
+      event.target.classList.toggle('more--expanded')
+
+      const listLI = event.target.closest('ul').querySelectorAll('li.item')
+
+      listLI.forEach((itemLI, index) => {
+        if (index > COUNT_OF_CATALOG_ITEMS_TO_SHOW) {
+          itemLI.classList.toggle('item--hidden')
+        }
+      })
+    }
+
+    item.addEventListener('click', onClickButtonMore)
+  })
+
+  // end catalog-in-header
+
   // закроем открытый popup (может быть только один открыт)
   const closePopup = function (event) {
     // console.log(event?.target, event?.currentTarget)
