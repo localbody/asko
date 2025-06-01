f3aaff6153ee6d46192dcb90bbc3f3349049c8c2
localbody macmini <localbody@gmail.com>
2025-06-01 14:05:57 +0300

    sliders

---
+++ dist/js/easy-slider.js
@@ -0,0 +1,21 @@
+// var swiper = new Swiper('.easy-slider', {
+//   pagination: {
+//     el: '.easy-slider__paginator',
+//   },
+// })
+
+const onLoad = () => {
+  console.log('onLoad')
+
+  const easySliders = document.querySelectorAll('.easy-slider')
+
+  easySliders.forEach((item) => {
+    new Swiper(item, {
+      pagination: {
+        el: item.querySelector('.easy-slider__paginator'),
+      },
+    })
+  })
+}
+
+document.addEventListener('DOMContentLoaded', onLoad)
--- dist/js/slider-project.js
+++
@@ -1,5 +0,0 @@
-var swiper = new Swiper('.project .swiper', {
-  pagination: {
-    el: '.project .slider__paginator',
-  },
-})
