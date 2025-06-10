aff754ca2e597a34712e15a873fb16a751626e9a
localbody-ic <localbody@mail.ru>
2025-06-10 13:57:50 +0300

    gulp css

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,27 @@ const ejs = require('gulp-ejs')
 const rename = require('gulp-rename')
 const plumber = require('gulp-plumber')
 const browserSync = require('browser-sync').create()
+const concat = require('gulp-concat')
+
+// Определяем порядок подключения CSS-файлов
+const cssFilesOrder = [
+  'src/styles/reset.css',
+  'src/styles/normalize.css',
+  'src/styles/colors.css',
+  'src/styles/fonts.css',
+  'src/styles/icons.css',
+  'src/styles/common.css',
+  'src/styles/components/*.css',
+  'src/styles/pages/*.css',
+  'src/styles/main.css',
+]
+
+function copyCSS() {
+  return gulp
+    .src(cssFilesOrder)
+    .pipe(concat('bundle.css')) // Объединяем в один файл
+    .pipe(gulp.dest('dist/css'))
+}
 
 // Компиляция EJS в HTML
 function compileEJS() {
@@ -25,7 +46,7 @@ function serve() {
 
   // Следим за изменениями в EJS и CSS
   gulp.watch('src/**/*.ejs', compileEJS)
-  // gulp.watch('src/styles/*.css', copyCSS)
+  gulp.watch('src/styles/*.css', copyCSS)
   gulp.watch('dist/*.html').on('change', browserSync.reload)
 }
 
@@ -39,4 +60,4 @@ function serve() {
 
 // Основная задача
 // exports.default = gulp.series(gulp.parallel(compileEJS, copyCSS), serve)
-exports.default = gulp.series(gulp.parallel(compileEJS), serve)
+exports.default = gulp.series(gulp.parallel(compileEJS, copyCSS), serve)
